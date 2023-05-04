c17285ab687632685d2a86a3a8f349d27861149d
abuzik999 <kdy11@mail.ru>
2023-05-04 11:10:35 +0500

    new commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int cost = 15000;
+        int each = 20;
+        int bonus = cost / each;
+
+        System.out.print(bonus + " бонусов. ");
+    }
+}
